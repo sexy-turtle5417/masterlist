35d5078aa4dda033e7ccf094cb28613511a15526
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-10-30 11:40:51 +0800

    feat: Add TrainingSession model
    Make it have a many-to-one relationship
    with Semester.

---
+++ app/Models/TrainingSession.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TrainingSession extends Model
+{
+    //
+    public $timestamps = false;
+}
---
+++ database/migrations/2025_10_30_030254_create_training_sessions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('training_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nth_session');
+            $table->foreignId('address_id')->constrained();
+            $table->foreignId('semester_id')->constrained();
+            $table->date('date_conducted');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->double('hours_credit');
+            $table->unique(['date_conducted', 'semester_id', 'nth_session']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('training_sessions');
+    }
+};
