593042012c4c48777f0410b0914a6ca745bd92f4
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-09 12:14:12 +0800

    fix: Make use of Model Factory to make Enrollments
    Make use of the standard model factories to make
    the code cleaner and more readable.

--- database/seeders/EnrollmentSeeder.php
+++ database/seeders/EnrollmentSeeder.php
@@ -2,11 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Cadet;
-use App\Models\Course;
 use App\Models\Enrollment;
-use App\Models\SchoolYear;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class EnrollmentSeeder extends Seeder
@@ -16,27 +12,8 @@ class EnrollmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-
-        $semester = SchoolYear::create([
-            "start" => 2024,
-            "end" => 2025
-        ])->semesters()->create([
-            "nth_semester" => 1
-        ]);
-
-        $course = Course::create([
-            "title" => "Military Science 1"
-        ]);
-
-        $cadets = Cadet::all();
-
-        foreach ($cadets as $cadet) {
-            Enrollment::create([
-                "course_id" => $course->id,
-                "semester_id" => $semester->id,
-                "cadet_id" => $cadet->id
-            ]);
-        }
+        Enrollment::factory()
+            ->count(100)
+            ->create();
     }
 }
