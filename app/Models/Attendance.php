88dd8bf0163d09f9904ad831a6a5da157cd9c327
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-09 12:51:00 +0800

    feat: Add HasFactory traits
    Add HasFactory traits to Attendance and
    TrainingSession models so that factories can be
    made for them.

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendance extends Model
 {
     //
+    use HasFactory;
+
     public function enrollment(): BelongsTo
     {
         return $this->belongsTo(Enrollment::class);
--- app/Models/TrainingSession.php
+++ app/Models/TrainingSession.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class TrainingSession extends Model
 {
     //
+    use HasFactory;
+
     public $timestamps = false;
 
     public function cadets(): BelongsToMany
