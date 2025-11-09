2eea15100bbed032767aa85232605bbfac11eab0
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-09 22:16:41 +0800

    refactor: Make EnrollmentSeeder more readable.

--- database/seeders/EnrollmentSeeder.php
+++ database/seeders/EnrollmentSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cadet;
+use App\Models\Course;
 use App\Models\Enrollment;
+use App\Models\Semester;
 use Illuminate\Database\Seeder;
 
 class EnrollmentSeeder extends Seeder
@@ -12,8 +15,35 @@ class EnrollmentSeeder extends Seeder
      */
     public function run(): void
     {
-        Enrollment::factory()
-            ->count(100)
-            ->create();
+        $cadets = Cadet::all();
+        $semesters = Semester::all();
+        $courses = Course::all();
+
+        $basicCadets = $cadets->take(90);
+        $advancedCadets = $cadets->skip(90);
+
+        foreach ($basicCadets as $cadet) {
+            foreach ($courses->whereIn('title', ["Military Science 1", "Military Science 2"]) as $index => $course) {
+                $semester = $semesters->get($index % $semesters->count());
+
+                Enrollment::factory()->create([
+                    'cadet_id' => $cadet->id,
+                    'course_id' => $course->id,
+                    'semester_id' => $semester->id,
+                ]);
+            }
+        }
+
+        foreach ($advancedCadets as $cadet) {
+            foreach ($courses as $index => $course) {
+                $semester = $semesters->get($index % $semesters->count());
+
+                Enrollment::factory()->create([
+                    'cadet_id' => $cadet->id,
+                    'course_id' => $course->id,
+                    'semester_id' => $semester->id,
+                ]);
+            }
+        }
     }
 }
