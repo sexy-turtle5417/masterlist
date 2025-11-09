15d87c0727abcf855daba51509d940dacbcea0fd
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-09 12:09:14 +0800

    feat (Models): Add HasFactory traits
    Add HasFactory trait to Enrollment, SchoolYear and
    Semester models.

--- app/Models/SchoolYear.php
+++ app/Models/SchoolYear.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class SchoolYear extends Model
 {
+    use HasFactory;
     //
     public function semesters(): HasMany
     {
