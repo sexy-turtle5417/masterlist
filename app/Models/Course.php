d7a45c284c95d97b65af1a3768cf570198179d4b
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-10 20:03:10 +0800

    feat: Add relationships between Models
    Add appropriate relationships between Course,
    Semester and CourseSemester.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     //
     use HasFactory;
+
+    public function courseSemesters(): HasMany
+    {
+        return $this->hasMany(CourseSemester::class);
+    }
+
+    public function semesters(): BelongsToMany
+    {
+        return $this->belongsToMany(Semester::class)->withTimestamps();
+    }
 }
--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Semester extends Model
 {
@@ -14,5 +16,15 @@ class Semester extends Model
     public function schoolYear(): BelongsTo
     {
         return $this->belongsTo(SchoolYear::class);
+    }
+
+    public function courseSemesters(): HasMany
+    {
+        return $this->hasMany(CourseSemester::class);
+    }
+
+    public function courses(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class)->withTimestamps();
     }
 }
