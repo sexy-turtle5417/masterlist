9ecd8632347b9d1051762df741f2d6dd809b198a
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-10 19:53:55 +0800

    feat: Add CourseSemester model and migrations
    Make CourseSemester serve as a intermediate table
    between Course and Semester.
    
    Create a belongs-to relation ship between
    CourseSemester and Course as well as
    with Semester.

---
+++ database/migrations/2025_11_10_115104_create_course_semester_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_semester', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained();
+            $table->foreignId('semester_id')->constrained();
+            $table->unique(['course_id', 'semester_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_semester');
+    }
+};
