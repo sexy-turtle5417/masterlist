c136a2c1a65d1ea3b565a6b5217d6823d477671f
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-10 20:29:54 +0800

    feat: Add has-many-through relationship to Course
    Add has-many-through relationship from Course
    model to Cadet model.
    
    We might want to find out how many cadets are
    enrolled in a course throughout the span of the
    entire organization.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Course extends Model
 {
@@ -20,5 +21,10 @@ class Course extends Model
     public function semesters(): BelongsToMany
     {
         return $this->belongsToMany(Semester::class)->withTimestamps();
+    }
+
+    public function cadets(): HasManyThrough
+    {
+        return $this->hasManyThrough(Cadet::class, CourseSemester::class);
     }
 }
