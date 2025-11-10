0bd321d4b7b322ce149974d68a90ddcc9f1e3a43
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-10 23:08:26 +0800

    feat: Add many-to-many relationship
    Add many-to-many relationship to CourseSemester
    and Student.

--- app/Models/CourseSemester.php
+++ app/Models/CourseSemester.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CourseSemester extends Model
@@ -25,5 +26,10 @@ class CourseSemester extends Model
     public function cadets(): HasMany
     {
         return $this->hasMany(Cadet::class);
+    }
+
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class, 'cadets');
     }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Student extends Model
 {
     /** @use HasFactory<\Database\Factories\StudentFactory> */
     use HasFactory;
+
+    public function courseSemesters(): BelongsToMany
+    {
+        return $this->belongsToMany(CourseSemester::class, 'cadets');
+    }
 }
