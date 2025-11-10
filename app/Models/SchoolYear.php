c0bb484201ca182f6c576f41244bf231cd6cdbd1
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-10 19:43:56 +0800

    feat: Add relationship to SchoolYear and Semester
    Add one-to-many relationship to SchoolYear
    and Semester such that a SchoolYear can have many
    Semesters but a Semester can only belong to
    one SchoolYear.

--- app/Models/SchoolYear.php
+++ app/Models/SchoolYear.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class SchoolYear extends Model
 {
     //
     use HasFactory;
+
+    public function semesters(): HasMany
+    {
+        return $this->hasMany(Semester::class);
+    }
 }
--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Semester extends Model
 {
     //
     use HasFactory;
+
+    public function schoolYear(): BelongsTo
+    {
+        return $this->belongsTo(SchoolYear::class);
+    }
 }
