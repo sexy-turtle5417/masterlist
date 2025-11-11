6a2fc497cab87421a4068775b6cb75d9a3e0ed7d
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-11 22:03:10 +0800

    feat: Add more relationships to Cadet model
    Add attendances and trainingSessions relationships
    and add hoursAttended method to find out the total
    hours a cadet has taken attendance.

--- app/Models/Cadet.php
+++ app/Models/Cadet.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cadet extends Model
 {
@@ -19,5 +21,20 @@ class Cadet extends Model
     public function student(): BelongsTo
     {
         return $this->belongsTo(Student::class);
+    }
+
+    public function attendances(): HasMany
+    {
+        return $this->hasMany(Attendance::class);
+    }
+
+    public function trainingSessions(): BelongsToMany
+    {
+        return $this->belongsToMany(TrainingSession::class, 'attendances');
+    }
+
+    public function hoursAttended(): float
+    {
+        return $this->attendances()->sum('hours_credit');
     }
 }
