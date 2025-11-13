50a8f91fe6e019047ef1ea978a95719659d5f8d4
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-11-13 22:45:52 +0800

    feat: Add total Strength count to data in SemesterController

--- app/Http/Controllers/SemesterController.php
+++ app/Http/Controllers/SemesterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Semester;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class SemesterController extends Controller
 {
@@ -38,14 +40,34 @@ class SemesterController extends Controller
     {
         //
         $cadets = $semester->cadets()
-            ->with('student')
-            ->with('courseSemester.course')
+            ->with([
+                'student',
+                'courseSemester.course'
+            ])
             ->withSum(['attendances as total_hours_attended'], 'hours_credit')
             ->withSum(['trainingSessions as total_training_hours'], 'hours_credit')
-            ->take(3)
+            ->take(10)
             ->get();
 
-        return view('semesters.show', ["cadets" => $cadets]);
+        $semesterCadets = $semester->cadets();
+
+        $totalCadetStrength = $semesterCadets->count();
+
+        $genderCounts = DB::table('semesters as sem')
+            ->join('course_semesters as cs', 'cs.semester_id', '=', 'sem.id')
+            ->join('cadets as c', 'c.course_semester_id', '=', 'cs.id')
+            ->join('students as s', 's.id', '=', 'c.student_id')
+            ->select('s.gender', DB::raw('COUNT(s.id) as total'))
+            ->where('sem.id', '=', $semester->id)
+            ->groupBy('s.gender')
+            ->pluck('total', 's.gender');
+
+        return view('semesters.show', [
+            "cadets" => $cadets,
+            "total_strength" => $totalCadetStrength,
+            "female_cadet_count" => $genderCounts['FEMALE'] ?? 0,
+            "male_cadet_count" => $genderCounts['MALE'] ?? 0
+        ]);
     }
 
     /**
